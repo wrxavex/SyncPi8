b38e22ee68b415f05c2410038dd11858bdb2905c
wrxavex <wrxavex@gmail.com>
2016-01-31 17:03:04 +0800

    modify button init state

--- RGB_LED_Tester.py
+++ RGB_LED_Tester.py
@@ -18,6 +18,14 @@ RGB = [RGB_RED, RGB_GREEN, RGB_BLUE]
 
 BTN1 = 23; BTN2 = 24
 BUTTON = [BTN1, BTN2]
+btn1_state_init = False
+btn2_state_init = False
+
+def button_init():
+    global btn1_state_init
+    global btn2_state_init
+    btn1_state_init = GPIO.input(BTN1)
+    btn2_state_init = GPIO.input(BTN2)
 
 def led_setup():
     for val1 in RGB:
@@ -47,23 +55,23 @@ def main():
     btn2_closed = True
     while True:
         btn1_state = GPIO.input(BTN1)
-        if btn1_state and btn1_closed and btn2_closed:
+        if btn1_state != btn1_state_init and btn1_closed and btn2_closed:
             print "blink start"
             RGB_blink(1)
             print "blink over"
             btn1_closed = False
-        elif btn1_state == False and btn1_closed == False:
+        elif btn1_state == btn1_state_init and btn1_closed == False:
             count1+=1
             print "CLOSE Button 1 %s times" % count1
             btn1_closed = True
 
         btn2_state = GPIO.input(BTN2)
-        if btn2_state and btn2_closed and btn1_closed:
+        if btn2_state != btn2_state_init and btn2_closed and btn1_closed:
             print "Call USB Video Sync Script"
             USB_Video_Sync()
             print "USB Video Sync Script"
             btn2_closed = False
-        elif btn2_state == False and btn2_closed == False:
+        elif btn2_state == btn2_state_init and btn2_closed == False:
             count2+=1
             print "CLOSE Button 2 %s times" % count2
             btn2_closed = True
