43d1fad9c80fd0af6f026b53419bc6b526ee533c
wrxavex <wrxavex@gmail.com>
2016-02-23 15:57:06 +0800

    add setting reading

--- pygameButtonTest.py
+++ pygameButtonTest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import sys
 import pygame
 import os
 import time
@@ -10,10 +10,15 @@ import RPi.GPIO as GPIO
 
 import get_ip
 
+my_ip = get_ip.myip
 
-myip = get_ip.myip
+
+def read_sync_setting(filename):
+    f = open(filename, 'r+w')
+    return f.read().split()
 
 
+player_setting = read_sync_setting('/boot/Sync_Setting.txt')
 
 # Set timezone
 
@@ -21,7 +26,7 @@ hostname = platform.node()
 os.environ['TZ'] = 'Asia/Taipei'
 time.tzset()
 
-fontfile = "/home/pi/SyncPi8/msjh.ttc"
+font_file = "/home/pi/SyncPi8/msjh.ttc"
 
 count = 0
 count1 = 0
@@ -47,12 +52,12 @@ lcd = pygame.display.set_mode((320, 240))
 lcd.fill((0, 0, 0))
 pygame.display.update()
 
-font_big = pygame.font.Font(fontfile, 72)
-font_small = pygame.font.Font(fontfile, 36)
-font_date = pygame.font.Font(fontfile, 24)
-font_hostname = pygame.font.Font(fontfile, 24)
-font_myip = pygame.font.Font(fontfile, 24)
-
+font_big = pygame.font.Font(font_file, 72)
+font_small = pygame.font.Font(font_file, 36)
+font_date = pygame.font.Font(font_file, 24)
+font_hostname = pygame.font.Font(font_file, 24)
+font_myip = pygame.font.Font(font_file, 24)
+font_setting = pygame.font.Font(font_file,24)
 
 def button_check(k):
     global button_pre
@@ -100,13 +105,16 @@ def main():
         lcd.fill((0, 0, 0))
         text_surface = font_date.render(u'%s' % timenow, True, WHITE)
         text_surface_hostname = font_hostname.render(u'%s' % hostname, True, WHITE)
-        text_surface_myip = font_myip.render(u'%s' % myip, True, WHITE)
+        text_surface_myip = font_myip.render(u'%s' % my_ip, True, WHITE)
+        text_surface_setting = font_setting.render(u'%s' % player_setting, True, WHITE)
         rect = text_surface.get_rect(center=(160, 200))
         rect_hostname = text_surface_hostname.get_rect(center=(160, 60))
         rect_myip = text_surface_myip.get_rect(center=(160, 100))
+        rect_setting = text_surface_setting.get_rect(center=(160,150))
         lcd.blit(text_surface, rect)
         lcd.blit(text_surface_hostname, rect_hostname)
         lcd.blit(text_surface_myip, rect_myip)
+        lcd.blit(text_surface_setting,rect_setting)
         pygame.display.update()
 
 
