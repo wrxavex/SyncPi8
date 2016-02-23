358c3fedc3b17e054fceab55e04d09c3bba0d291
wrxavex <wrxavex@gmail.com>
2016-02-23 21:43:48 +0800

    更新顯示資訊

--- Slave.sh
+++ Slave.sh
@@ -3,14 +3,15 @@
 
 SERVICE='omxplayer-sync'
 while true; do
+now=$(date)
 if ps ax | grep -v grep | grep $SERVICE > /dev/null
 then
-echo "running" #>>/dev/null
+echo "$now SyncVideo Slave running" #>>/dev/null
 else
+echo "Error--$now"
 omxplayer-sync -b -l /home/pi/SyncPi8/SyncVideo.mp4 &
 
 fi
 done
 
 #---- end slave script ----
-w
