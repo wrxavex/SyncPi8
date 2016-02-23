8356753f97482f9a315c48a53b42f5fe1e74bd93
wrxavex <wrxavex@gmail.com>
2016-02-23 14:44:30 +0800

    change to module

--- pygameButtonTest.py
+++ pygameButtonTest.py
@@ -7,20 +7,13 @@ import time
 import platform
 from time import sleep
 import RPi.GPIO as GPIO
-import socket
-import fcntl
-import struct
+
+import get_ip
 
 
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-    )[20:24])
+myip = get_ip.myip
 
-myip = get_ip_address('eth0')
+
 
 # Set timezone
 
