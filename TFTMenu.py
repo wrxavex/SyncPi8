47b76db9438627f724751058012cf6c8fe05f2f1
wrxavex <wrxavex@gmail.com>
2016-02-23 16:54:51 +0800

    display new video status

--- TFTMenu.py
+++ TFTMenu.py
@@ -71,9 +71,8 @@ def button_check(k):
     global count4
     if k == 23 and button_pre != 23:
         count1 += 1
-        text_surface_have_new_video = font.have_new_video.render(u'%s' % VS.NewVideoFile, True, WHITE)
-        rect_have_new_video = text_surface.get_rect(center=(160, 160))
-        lcd.blit(text_surface_have_new_video, rect_have_new_video)
+        lcd.fill(0,0,0)
+
         pygame.display.update()
         sleep(3)
         return count1
@@ -115,18 +114,25 @@ def main():
         timenow = time.strftime('%Z %x %X')
 
         lcd.fill((0, 0, 0))
+
         text_surface = font_date.render(u'%s' % timenow, True, WHITE)
         text_surface_hostname = font_hostname.render(u'%s' % hostname, True, WHITE)
         text_surface_myip = font_myip.render(u'%s' % my_ip, True, WHITE)
         text_surface_setting = font_setting.render(u'%s' % player_setting, True, WHITE)
-        rect = text_surface.get_rect(center=(160, 200))
+        text_surface_have_new_video = font_have_new_video.render(u'New Video is %s' % VS.NewVideoFile, True, WHITE)
+
+        rect = text_surface.get_rect(center=(160, 220))
         rect_hostname = text_surface_hostname.get_rect(center=(160, 60))
-        rect_myip = text_surface_myip.get_rect(center=(160, 100))
-        rect_setting = text_surface_setting.get_rect(center=(160,150))
+        rect_myip = text_surface_myip.get_rect(center=(160, 80))
+        rect_setting = text_surface_setting.get_rect(center=(160,100))
+        rect_have_new_video = text_surface_have_new_video.get_rect(center=(160, 120))
+
         lcd.blit(text_surface, rect)
         lcd.blit(text_surface_hostname, rect_hostname)
         lcd.blit(text_surface_myip, rect_myip)
         lcd.blit(text_surface_setting,rect_setting)
+        lcd.blit(text_surface_have_new_video, rect_have_new_video)
+
         pygame.display.update()
 
 
