51e502d2d76af624c3e229fb7a3178ebccd3cf43
wrxavex <wrxavex@gmail.com>
2016-06-04 00:56:33 +0800

    videostatus test

--- flaskServer.py
+++ flaskServer.py
@@ -2,15 +2,17 @@ from flask import Flask
 import os
 import time
 import subprocess
+import NoButtonVideoSync as VideoSync
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     cpu_temp_raw_data = subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"])
-    get_cpu_temp = cpu_temp_raw_data.strip()
-    return get_cpu_temp
+    get_cpu_temp = cpu_temp_raw_data.strip
+    videostatus = VideoSync.main()
+
+    return videostatus
 
 
 @app.route("/video")
