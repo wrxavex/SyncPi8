4b76b48e8976d0be581fe4dd5b4a9c523e08784f
wrxavex <wrxavex@gmail.com>
2016-06-04 10:48:20 +0800

    template test

--- flaskServer.py
+++ flaskServer.py
@@ -13,8 +13,8 @@ def home(name=None):
     cpu_temp_raw_data = subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"])
     get_cpu_temp = cpu_temp_raw_data.strip
     videostatus = VideoSync.main(VideoSync.usb_video_file)
-
-    return render_template('home.html', name=str(get_cpu_temp))
+    nano = get_cpu_temp
+    return render_template('home.html', name=name)
 
 
 @app.route("/video")
