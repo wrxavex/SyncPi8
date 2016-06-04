fa6f78793d70e951f659b8b7209c7fb13f6b9344
wrxavex <wrxavex@gmail.com>
2016-06-04 16:10:16 +0800

    fix check_omx_status

--- set_ip_address.py
+++ set_ip_address.py
@@ -8,12 +8,16 @@ ip_config = ip_config_file.read()
 print('ip config: %s' % ip_config)
 ip_config_file.close()
 
+print(0 < id_to_set > 255)
+
 if 0 < id_to_set > 255:
     new_ip_config = ip_config.replace('replace_id_here', 'id_to_set')
     print('new ip config: %s'% new_ip_config)
     ip_set_file = open('/etc/dhcpcd.conf', 'w')
     ip_set_file.write(new_ip_config)
     ip_set_file.close()
+else:
+    print('id not a valid number')
 
 
 
