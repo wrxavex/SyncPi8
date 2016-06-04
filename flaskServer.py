c3acc06ab191673b6ad97f1c6be0fe9b6f2ad29a
wrxavex <wrxavex@gmail.com>
2016-06-04 11:21:40 +0800

    template test

--- flaskServer.py
+++ flaskServer.py
@@ -9,16 +9,16 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<name>")
-def home(name=None, videoinLocalSize=None, videoinUsbSize=None):
+def home(cputemp=None, result=None, videoinLocalSize=None, videoinUsbSize=None):
     cpu_temp_raw_data = subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"])
     get_cpu_temp = cpu_temp_raw_data.strip()
     videostatus = VideoSync.main(VideoSync.usb_video_file)
     print ("name before: %s " % name)
-    name = VideoSync.result
+    result = VideoSync.result
     videoinLocalSize = VideoSync.videoinLocalSize
     videoinUsbSize = VideoSync.videoinUsbSize
     print ("name after: %s" % name)
-    return render_template('home.html', name=name, videoinLocalSize=videoinLocalSize, videoinUsbSize=videoinUsbSize)
+    return render_template('home.html', cputemp=get_cpu_temp, result=result, videoinLocalSize=videoinLocalSize, videoinUsbSize=videoinUsbSize)
 
 
 @app.route("/video")
