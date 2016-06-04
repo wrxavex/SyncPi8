d59f618bd43f9f77508c5e2b5877a88ff44f01fc
wrxavex <wrxavex@gmail.com>
2016-06-04 10:34:20 +0800

    template test

--- flaskServer.py
+++ flaskServer.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 import os
 import time
 import subprocess
@@ -7,12 +8,13 @@ import NoButtonVideoSync as VideoSync
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+@app.route("/<name>")
+def home(name=None):
     cpu_temp_raw_data = subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"])
     get_cpu_temp = cpu_temp_raw_data.strip
     videostatus = VideoSync.main(VideoSync.usb_video_file)
 
-    return 'VideoStatus:%s ' % videostatus
+    return render_template('home.html', name=name)
 
 
 @app.route("/video")
