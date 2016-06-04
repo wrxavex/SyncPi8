8a322854585dab12b97a4ef588f5cdad740aa364
wrxavex <wrxavex@gmail.com>
2016-06-04 20:38:18 +0800

    change ip_conf location

--- set_ip_address.py
+++ set_ip_address.py
@@ -1,19 +1,18 @@
 id_file = open('set_id', 'r')
-id_to_set = int(id_file.read().strip())
-print('id to set: %d' % id_to_set)
+id_to_set = id_file.read().strip()
+print('id to set: %s' % id_to_set)
 id_file.close()
 
 ip_config_file = open('ip_conf_sample', 'r')
 ip_config = ip_config_file.read()
-print('ip config: %s' % ip_config)
 ip_config_file.close()
 
-print(0 < id_to_set < 255)
 
-if 200 < id_to_set < 255:
-    new_ip_config = ip_config.replace('replace_id_here', int(id_to_set))
+if 200 < int(id_to_set) < 255:
+    new_ip_config = ip_config.replace('replace_id_here', id_to_set)
     ip_set_file = open('/etc/dhcpcd.conf', 'w')
     ip_set_file.write(new_ip_config)
+    print("new config: %s" % new_ip_config)
     ip_set_file.close()
 else:
     print('id not a valid number')
