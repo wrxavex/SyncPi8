742805e70a3a86c413f04d7393e3f2c7f0b0a7ca
wrxavex <wrxavex@gmail.com>
2016-06-04 11:40:42 +0800

    template test

--- flaskServer.py
+++ flaskServer.py
@@ -9,14 +9,15 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<name>")
-def home(name=None, cputemp=None, result=None, videoinLocalSize=None, videoinUsbSize=None):
+def home(name=None, cputemp=None, result=None, videoinLocalSize=None, videoinUsbSize=None, sync_status=None):
     cpu_temp_raw_data = subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"])
+    sync_status_result = subprocess.check_output(["ps", "ax", "|", "grep", "-v", "grep", "|", "grep", "omxplayer-sync"])
     get_cpu_temp = cpu_temp_raw_data.strip()
     videostatus = VideoSync.main(VideoSync.usb_video_file)
     result = VideoSync.result
     videoinLocalSize = VideoSync.videoinLocalSize
     videoinUsbSize = VideoSync.videoinUsbSize
-    return render_template('home.html',name=name, cputemp=get_cpu_temp, result=result, videoinLocalSize=videoinLocalSize, videoinUsbSize=videoinUsbSize)
+    return render_template('home.html',name=name, cputemp=get_cpu_temp, result=result, videoinLocalSize=videoinLocalSize, videoinUsbSize=videoinUsbSize, sync_status=sync_status_result)
 
 
 @app.route("/video")
