f77886fb2a1d3c98229778ae4a69e4786185034a
wrxavex <wrxavex@gmail.com>
2016-06-04 12:01:40 +0800

    fix check_omx_status

--- flaskServer.py
+++ flaskServer.py
@@ -11,7 +11,8 @@ app = Flask(__name__)
 @app.route("/<name>")
 def home(name=None, cputemp=None, result=None, videoinLocalSize=None, videoinUsbSize=None, sync_status=None):
     cpu_temp_raw_data = subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"])
-    sync_status_result = subprocess.Popen(['sh', 'check_omx_status.sh'])
+    sync_status_result = subprocess.check_output(['sh', 'check_omx_status.sh'])
+    # sync_status_result = subprocess.Popen(['sh', 'check_omx_status.sh'])
     get_cpu_temp = cpu_temp_raw_data.strip()
     VideoSync.main(VideoSync.usb_video_file)
     result = VideoSync.result
